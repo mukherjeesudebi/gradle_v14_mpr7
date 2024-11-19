ad8892554b9f7af2b888c89741300de2f61dca99
mukherjeesudebi <sudebi@vaadin.com>
2024-11-19 19:38:18 +0200

    jsoup version forced

--- app/src/main/java/org/gradle/samples/SpringBootDemoApplication.java
+++ app/src/main/java/org/gradle/samples/SpringBootDemoApplication.java
@@ -2,9 +2,12 @@ package org.gradle.samples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
