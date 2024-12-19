37b703fdadd2aaa0dabcf23184e3b402b624adb3
mukherjeesudebi <sudebi@vaadin.com>
2024-12-19 11:20:52 +0200

    added EnableVaadin annotation

--- app/src/main/java/org/gradle/samples/SpringBootDemoApplication.java
+++ app/src/main/java/org/gradle/samples/SpringBootDemoApplication.java
@@ -1,10 +1,14 @@
 package org.gradle.samples;
 
+import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages ="org.gradle.samples" )
+@EnableVaadin({"org.gradle.samples"})
 public class SpringBootDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
