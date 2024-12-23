6e0c7264b260566daa32baeb6a2595321e6484b8
mukherjeesudebi <sudebi@vaadin.com>
2024-12-23 16:33:51 +0200

    added sample-am module

---
+++ sample-am/sample-am-ui/src/main/java/org/gradle/samples/ChartView.java
@@ -0,0 +1,79 @@
+package org.gradle.samples;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.*;
+import com.vaadin.addon.charts.model.style.SolidColor;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.mpr.LegacyWrapper;
+import com.vaadin.mpr.core.HasLegacyComponents;
+import com.vaadin.server.StreamResource;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.Notification;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Route("chart-view")
+public class ChartView extends VerticalLayout implements HasLegacyComponents {
+    public ChartView(){
+        Chart chart = new Chart(ChartType.BAR);
+
+        com.vaadin.addon.charts.model.Configuration conf = chart.getConfiguration();
+
+        conf.setTitle("Historic World Population by Region");
+        conf.setSubTitle("Source: Wikipedia.org");
+
+        XAxis x = new XAxis();
+        x.setCategories("Africa", "America", "Asia", "Europe", "Oceania");
+        x.setTitle((String) null);
+        conf.addxAxis(x);
+
+        YAxis y = new YAxis();
+        y.setMin(0);
+        AxisTitle title = new AxisTitle("Population (millions)");
+        title.setAlign(VerticalAlign.MIDDLE);
+        y.setTitle(title);
+        conf.addyAxis(y);
+
+        Tooltip tooltip = new Tooltip();
+        tooltip.setFormatter("this.series.name +': '+ this.y +' millions'");
+        conf.setTooltip(tooltip);
+
+        PlotOptionsBar plot = new PlotOptionsBar();
+        plot.setDataLabels(new DataLabels(true));
+        conf.setPlotOptions(plot);
+
+        Legend legend = new Legend();
+        legend.setLayout(LayoutDirection.VERTICAL);
+        legend.setAlign(HorizontalAlign.RIGHT);
+        legend.setVerticalAlign(VerticalAlign.TOP);
+        legend.setX(-100);
+        legend.setY(100);
+        legend.setFloating(true);
+        legend.setBorderWidth(1);
+        legend.setBackgroundColor(new SolidColor("#FFFFFF"));
+        legend.setShadow(true);
+        conf.setLegend(legend);
+
+        conf.disableCredits();
+
+        List<Series> series = new ArrayList<Series>();
+        series.add(new ListSeries("Year 1800", 107, 31, 635, 203, 2));
+        series.add(new ListSeries("Year 1900", 133, 156, 947, 408, 6));
+        series.add(new ListSeries("Year 2008", 973, 914, 4054, 732, 34));
+        conf.setSeries(series);
+
+        chart.drawChart(conf);
+        add(chart);
+    }
+
+}
